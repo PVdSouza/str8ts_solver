1f7735b7215a00ad53e669019c5a0e1156c937d0
Pedro Souza <pedro.j.v.souza@gmail.com>
2020-10-10 17:22:22 -0300

    getAreas done

--- main.hs
+++ main.hs
@@ -19,6 +19,8 @@ boolMatrix = array ((0,0),(5,5)) [((0,0),True), ((0,1),False), ((0,2),False), ((
                                   ((4,0),False), ((4,1),False), ((4,2),False), ((4,3),False), ((4,4),False), ((4,5),True),
                                   ((5,0),True), ((5,1),True), ((5,2),True), ((5,3),False), ((5,4),False), ((5,5),True)]
 
+
+
 getRow :: Array (Int,Int) Int -> Int -> Int -> [Int]
 getRow mat a 6 = [] --gambiarra pois n sei fazer condição de parada em haskell
 getRow mat a b = if mat!(a,b) /= -1 then [mat!(a,b)] ++ getRow mat a (b+1)
@@ -41,12 +43,46 @@ isBlank :: Array (Int,Int) Int -> Array (Int,Int) Bool -> Int -> Int -> Bool
 isBlank matNum matBool a b = matNum!(a,b) == -1 && matBool!(a,b) /= True
 
 isSequence :: [Int] -> Bool
-isSequence list = (((sort list) !! (length list)) - ((sort list) !! 0)) == length list
+isSequence list = (((sort list) !! (length list - 1)) - ((sort list) !! 0)) == length list - 1
 
+
+getAreasRow :: Array (Int, Int) Bool -> Int -> Int -> [(Int, Int)]
+getAreasRow mat a 6 = []
+getAreasRow mat a b  | mat!(a,b) = getAreasRow mat a (b+1)
+                     | otherwise = (a,b) : getAreasRow mat a (b+1)
+
+getAreasColumn :: Array (Int, Int) Bool -> Int -> Int -> [(Int, Int)]
+getAreasColumn mat 6 a = []
+getAreasColumn mat a b  | mat!(a,b) = getAreasRow mat (a+1) b
+                        | otherwise = (a,b) : getAreasRow mat (a+1) b                     
+
+getSequenceRow :: [(Int, Int)] ->[[(Int, Int)]]
+getSequenceRow ((a,b):(c,d):xs) | (a == c-1) = [(a,b)] : getSequenceRow ((c,d):xs)
+                                | otherwise  = getSequenceRow ((c,d):xs)
+getSequenceRow ((a,b):()) = []
 
 
 main = do
-    print (getRow matrix 0 1)
+
+    --pega os brancos de cada linha
+    print (getSequenceRow(getAreasRow boolMatrix 0 0))
+    print (getSequenceRow(getAreasRow boolMatrix 1 0))
+    print (getSequenceRow(getAreasRow boolMatrix 2 0))
+    print (getSequenceRow(getAreasRow boolMatrix 3 0))
+    print (getSequenceRow(getAreasRow boolMatrix 4 0))
+    print (getSequenceRow(getAreasRow boolMatrix 5 0))
+    print("")
+     --pega os brancos de cada coluna
+    print (getAreasColumn boolMatrix 0 0)
+    print (getAreasColumn boolMatrix 0 1)
+    print (getAreasColumn boolMatrix 0 2)
+    print (getAreasColumn boolMatrix 0 3)
+    print (getAreasColumn boolMatrix 0 4)
+    print (getAreasColumn boolMatrix 0 5)
+    print("")
+    
+    print("")
+
     print (getColumn matrix 0 1)
     print $ possibleNum matrix 0 1
     print $ isSequence [4,3]
