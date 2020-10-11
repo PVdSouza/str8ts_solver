71a6fb157ca494b6c3f53169de84f448a29ee673
Pedro Souza <pedro.j.v.souza@gmail.com>
2020-10-10 21:34:10 -0300

    added remover

--- main.hs
+++ main.hs
@@ -16,38 +16,49 @@ colorBoard = [False, True,  True,  False, False, False,
               True,  True,  True,  True,  True,  False,
               False, False, False, True,  True,  False]
 
--- indice para coordenaqda
+-- transformador de indice para coordenaqda
 itop :: Int -> (Int, Int)
-itop index = (coordX i, coordY i)
-    where coordX i = i - 9 * (i `div` 9)
-          coordY i = i `div` 9
+itop index = (coordX index, coordY index)
+    where coordX index = index - 9 * (index `div` 9)
+          coordY index = index `div` 9
 
--- coordenada para indice
+-- trasformador coordenada para indice
 ptoi :: (Int, Int) -> Int
 ptoy (x, y) = x + y * 9
 
 -- pega as colunas de numberBoard ou colorBoard
 getColumn :: Int -> [t] -> [t]
 getColumn index board = auxGet (itop index) board
-    where auxGet (x, _) board = map (\y -> board !! ptoi (x, y)) [0..8]
+    where auxGet (x, _) board = map (\y -> board !! ptoi (x, y)) [0..5]
 
 -- pega as linhas de numberBoard ou colorBoard
 getRow :: Int -> [Int] -> [Int]
 getRow index board = auxGet (itop index) board
-    where auxGet (_, y) board = map (\x -> s !! ptoi (x, y)) [0..8]
+    where auxGet (_, y) board = map (\x -> s !! ptoi (x, y)) [0..5]
 
+-- remove elementos de uma uma lista que estao presentes na segunda lista
+remove :: [Int] -> [Int] -> [Int]
+remove [] _      = []
+remove values [] = values
+remove xs (y:ys) = actRemove (actRemove y xs) ys
+
+-- remove as ocorrencias de a em uma lista
+actRemove :: Int -> [Int] -> [Int]
+actRemove _ [] = []
+actRemove a (x:xs) | x == a    = actRemove a xs
+                   | otherwise = x : actRemove a xs
+
+-- valores validos no indice index 
 getOptions :: Int -> [Int] -> [Int]
-
--- tamanho de array
-len :: [t] -> Int
-len [] = 0
-len (_ : b) = 1 + len b
+getOptions options board | index > length board  = []
+                         | (board !! index) == 0 = [1..6] `remove` (getColumn index board ++ getRow index board) -- ++ getStraights index board
+                         | otherwise             = [s !! p]
 
 -- busca a proxima celular nao preenchida do tabuleiro
 nextBlank :: Int -> [Int] -> Int
-nextBlank index board | p = ((len board) - 1)     = ((len board) - 1)
-                      | board !! (index + 1) == 0 = index + 1
-                      | otherwise                 = nextBlank (index + 1) board
+nextBlank index board | index == ((length board) - 1)     = ((length board) - 1)
+                      | board !! (index + 1) == 0         = index + 1
+                      | otherwise                         = nextBlank (index + 1) board
 
 -- cria um novo tabuleiro substituindo o valor "value" no indice "index"
 try :: Int -> [Int] -> Int -> [Int]
