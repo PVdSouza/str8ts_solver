a379b055e2a03f39114885ed0910d781743c2d92
Nicole Schmidt <nicoleschmidt2000@gmail.com>
2020-11-06 22:24:03 -0300

    Add getSequence, getRow, getLine, SplitOn functions.

--- main.lisp
+++ main.lisp
@@ -1,3 +1,19 @@
+(set 'numberBoard '(3 0 0 4 0 0
+                    0 0 0 0 0 0
+                    0 0 0 0 0 2
+                    4 0 0 0 0 3
+                    0 0 0 0 0 5
+                    0 0 3 0 1 0))
+
+
+(set 'colorBoard '(NIL NIL T  T  T  NIL
+                   NIL T   T  T  T  NIL
+                   T   T  NIL NIL T  T
+                   T   T  NIL NIL T  T
+                   NIL T  T  T  T   NIL
+                   NIL T  T  T  NIL NIL))
+
+
 (defun itop (index)
     (cons
         (multiple-value-bind (q r) (floor index 6) r)
@@ -16,4 +32,74 @@
         isValid (+ index 1)
         index
     )
+)
+
+(defun getRow(index board)
+    (mapcar (lambda (x) (nth (ptoi x index) board)) '(0 1 2 3 4 5))
+)
+
+(defun getColumn(index board)
+    (mapcar (lambda (y) (nth (ptoi index y) board)) '(0 1 2 3 4 5))
+)
+
+(defun getSequences (board color index)
+    (if (= index 6)
+        (cons (cons (getSeqColumn board color index) (getSeqLine board color index)) (getSequences board color (+ index 1)))
+    )
+)
+
+(defun filter-nil (lista)
+    (remove-if #'(lambda (x) (not x)) lista)
+)
+
+(defun splitOn (lista)
+    (if (null lista)
+        ()
+        (filter-nil (cons (take-while-second lista) (splitOn (drop-while-not (drop-while lista)))))
+    )
+)
+
+(defun drop-while-not (lista)
+    (if (null lista)
+        ()
+        (if (not (nth 1 (car lista)))
+            (drop-while-not (cdr lista))
+            lista
+        )
+    )
+)
+
+(defun drop-while (lista)
+    (if (null lista)
+        ()
+        (if (nth 1 (car lista))
+            (drop-while(cdr lista))
+            lista
+        )
+    )
+)
+
+(defun take-while-second(lista)
+    (loop for item in lista
+          while (nth 1 item)
+          collect (nth 0 item))
+    ;; (collect-list (list i) (for i in lista) (take-while ()))
+)
+
+(defun take-while (pred list)
+    (loop for x in list
+        while (funcall pred x)
+        collect x)
+)
+
+(defun getSeqColumn ()
+)
+
+(defun getSeqLine()
+)
+
+(defun isFinished()
+)
+
+(defun isSequence()
 )
